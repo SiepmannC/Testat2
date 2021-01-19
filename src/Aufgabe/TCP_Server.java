ab524697cad9c4d70e7c455181f351b6c1c32668
Davo00 <m.davit@protonmail.com>
2021-01-19 11:18:35 +0100

    Server Version 0.1

--- src/Aufgabe/TCP_Server.java
+++ src/Aufgabe/TCP_Server.java
@@ -1,5 +1,40 @@
 package Aufgabe;
 
-public class TCP_Server {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
 
+public class TCP_Server {
+    public final static int DEFAULT_PORT = 7777;
+
+    public static void main(String[] args) {
+        int port = DEFAULT_PORT;
+        PrintWriter out = null;
+        Socket connection = null;
+        try {
+            ServerSocket server = new ServerSocket(port);
+            while (true) {
+                try {
+                    connection = server.accept();
+                    out = new PrintWriter(connection.getOutputStream());
+                    Date now = new Date();
+                    out.println(now.toString());
+                    out.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    try {
+                        if (connection != null) connection.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
 }
